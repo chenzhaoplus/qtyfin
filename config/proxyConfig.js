f00f50e6e773f1d506008738e76c1854689226d1
chenzhao <chenzhaoplus@163.com>
2021-08-10 10:40:55 +0800

    设置代理

---
+++ config/proxyConfig.js
@@ -0,0 +1,14 @@
+// 参考： https://segmentfault.com/a/1190000011007043
+
+module.exports = {
+  proxy: {
+        '/': {    //将www.exaple.com印射为/apis
+            target: 'http://localhost:5000',  // 接口域名
+            secure: false,  // 如果是https接口，需要配置这个参数
+            changeOrigin: true,  //是否跨域
+            pathRewrite: {
+                '^/': ''   //需要rewrite的,
+            }
+        }
+  }
+}
